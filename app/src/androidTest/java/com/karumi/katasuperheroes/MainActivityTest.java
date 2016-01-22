7cef96e39a3bdf15049c1532c71c9eb49c81806e
Rafa Vázquez <rafa.vazsan@gmail.com>
2016-01-22 12:57:44 +0100

    Added test for opening detail activity intent

--- app/src/androidTest/java/com/karumi/katasuperheroes/MainActivityTest.java
+++ app/src/androidTest/java/com/karumi/katasuperheroes/MainActivityTest.java
@@ -18,6 +18,7 @@ package com.karumi.katasuperheroes;
 
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -29,6 +30,7 @@ import com.karumi.katasuperheroes.model.SuperHero;
 import com.karumi.katasuperheroes.model.SuperHeroesRepository;
 import com.karumi.katasuperheroes.recyclerview.RecyclerViewInteraction;
 import com.karumi.katasuperheroes.ui.view.MainActivity;
+import com.karumi.katasuperheroes.ui.view.SuperHeroDetailActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -42,7 +44,11 @@ import java.util.List;
 import it.cosenonjaviste.daggermock.DaggerMockRule;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -140,6 +146,20 @@ public class MainActivityTest {
                 });
     }
 
+    @Test
+    public void opensSuperHeroDetailWhenRowClicked() throws Exception {
+        List<SuperHero> superHeros = givenThereAreSomeSuperHeroes();
+        int clickIndex = 0;
+        SuperHero selectedSuperHero = superHeros.get(clickIndex);
+
+        startActivity();
+        onView(withId(R.id.recycler_view)).
+                perform(RecyclerViewActions.actionOnItemAtPosition(clickIndex, click()));
+
+        intended(hasComponent(SuperHeroDetailActivity.class.getCanonicalName()));
+        intended(hasExtra("super_hero_name_key", selectedSuperHero.getName()));
+    }
+
     private void givenThereAreSomeAvengers() {
         when(repository.getAll()).thenReturn(superHeroListOf(10, true));
     }
@@ -148,12 +168,18 @@ public class MainActivityTest {
         when(repository.getAll()).thenReturn(Collections.<SuperHero>emptyList());
     }
 
-    private void givenThereAreSomeSuperHeroes() {
-        givenThereAreSomeSuperHeroes(10);
+    private List<SuperHero> givenThereAreSomeSuperHeroes() {
+        return givenThereAreSomeSuperHeroes(10);
     }
 
-    private void givenThereAreSomeSuperHeroes(int count) {
-        when(repository.getAll()).thenReturn(superHeroListOf(count));
+    private List<SuperHero> givenThereAreSomeSuperHeroes(int count) {
+        List<SuperHero> heroes = superHeroListOf(count);
+        when(repository.getAll()).thenReturn(heroes);
+
+        for (SuperHero hero : heroes) {
+            when(repository.getByName(hero.getName())).thenReturn(hero);
+        }
+        return heroes;
     }
 
     private List<SuperHero> superHeroListOf(int count) {
